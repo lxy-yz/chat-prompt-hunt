8271d7ef42d8bf4a23e9fe799f3a449771962ac7
lxy-yz <lxy.dev.yz@gmail.com>
2023-06-17 09:47:39 -0700

    landing

---
+++ app/landing/page.tsx
@@ -0,0 +1,336 @@
+export default function Page() {
+  return (
+    <div className="bg-gray-900">
+      <main>
+        <div className="relative overflow-hidden isolate">
+          <svg
+            className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
+                width={200}
+                height={200}
+                x="50%"
+                y={-1}
+                patternUnits="userSpaceOnUse"
+              >
+                <path d="M.5 200V.5H200" fill="none" />
+              </pattern>
+            </defs>
+            <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+              <path
+                d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                strokeWidth={0}
+              />
+            </svg>
+            <rect
+              width="100%"
+              height="100%"
+              strokeWidth={0}
+              fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
+            />
+          </svg>
+          <div
+            className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+              style={{
+                clipPath:
+                  "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
+              }}
+            />
+          </div>
+          <div className="px-6 pt-10 pb-24 mx-auto max-w-7xl sm:pb-40 lg:px-8 lg:pt-40">
+            <div className="max-w-3xl mx-auto text-center lg:pt-8">
+              <div className="flex items-center justify-center space-x-3 text-white">
+                <img
+                  alt=""
+                  loading="lazy"
+                  width={44}
+                  height={44}
+                  decoding="async"
+                  data-nimg={1}
+                  className="rounded-full"
+                  style={{ color: "transparent" }}
+                  srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4033a551.png&w=48&q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4033a551.png&w=96&q=75 2x"
+                  src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.4033a551.png&w=96&q=75"
+                />
+                <span className="text-lg font-black">TubeCrisp</span>
+              </div>
+              <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Turn your videos into text content such as
+                <br />
+                blogs, newsletters, short posts
+                <br />
+                and Twitter threads automatically.
+              </h1>
+              <p className="mt-6 text-lg font-medium leading-8 text-gray-300">
+                With TubeCrisp, your videos become engaging blog posts, newsletters,
+                and Twitter threads in just a few clicks.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 mx-auto mt-32 max-w-7xl sm:mt-0 lg:px-8">
+          <div className="max-w-2xl mx-auto text-center">
+            <h2 className="text-base font-semibold leading-7 text-indigo-400">
+              Promoting text content matters
+            </h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Everything you need to spread your content
+            </p>
+          </div>
+          <div className="max-w-2xl mx-auto mt-16 sm:mt-20 lg:mt-24 lg:max-w-none">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+              <div className="flex flex-col">
+                <dt className="text-base font-semibold leading-7 text-white">
+                  <div className="flex items-center justify-center w-10 h-10 mb-6 bg-indigo-500 rounded-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                      className="w-6 h-6 text-white"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                      />
+                    </svg>
+                  </div>
+                  Efficiency
+                </dt>
+                <dd className="flex flex-col flex-auto mt-1 text-base leading-7 text-gray-300">
+                  <p className="flex-auto">
+                    Simplify your content creation process by reusing video content.
+                  </p>
+                </dd>
+              </div>
+              <div className="flex flex-col">
+                <dt className="text-base font-semibold leading-7 text-white">
+                  <div className="flex items-center justify-center w-10 h-10 mb-6 bg-indigo-500 rounded-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                      className="w-6 h-6 text-white"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                      />
+                    </svg>
+                  </div>
+                  Expand Your Reach
+                </dt>
+                <dd className="flex flex-col flex-auto mt-1 text-base leading-7 text-gray-300">
+                  <p className="flex-auto">
+                    Share your message across different platforms with automatically
+                    generated text content.
+                  </p>
+                </dd>
+              </div>
+              <div className="flex flex-col">
+                <dt className="text-base font-semibold leading-7 text-white">
+                  <div className="flex items-center justify-center w-10 h-10 mb-6 bg-indigo-500 rounded-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                      className="w-6 h-6 text-white"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                      />
+                    </svg>
+                  </div>
+                  Better Accessibility
+                </dt>
+                <dd className="flex flex-col flex-auto mt-1 text-base leading-7 text-gray-300">
+                  <p className="flex-auto">
+                    Provide an alternative way for your audience to engage with your
+                    content.
+                  </p>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        <div className="relative px-6 py-32 mt-32 isolate sm:mt-0 sm:py-40 lg:px-8">
+          <svg
+            className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="1d4240dd-898f-445f-932d-e2872fd12de3"
+                width={200}
+                height={200}
+                x="50%"
+                y={0}
+                patternUnits="userSpaceOnUse"
+              >
+                <path d="M.5 200V.5H200" fill="none" />
+              </pattern>
+            </defs>
+            <svg x="50%" y={0} className="overflow-visible fill-gray-800/20">
+              <path
+                d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                strokeWidth={0}
+              />
+            </svg>
+            <rect
+              width="100%"
+              height="100%"
+              strokeWidth={0}
+              fill="url(#1d4240dd-898f-445f-932d-e2872fd12de3)"
+            />
+          </svg>
+          <div
+            className="absolute inset-x-0 flex justify-center overflow-hidden top-10 -z-10 transform-gpu blur-3xl"
+            aria-hidden="true"
+          >
+            <div
+              className="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+              style={{
+                clipPath:
+                  "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
+              }}
+            />
+          </div>
+          <div className="max-w-2xl mx-auto text-center">
+            <span className="inline-block px-3 py-1 mb-8 text-sm font-bold leading-6 text-indigo-400 rounded-full bg-indigo-500/10 ring-1 ring-inset ring-indigo-500/20">
+              Pre-sale offer — first 100 purchases
+            </span>
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Get TubeCrisp without subscription.
+            </h2>
+            <p className="max-w-xl mx-auto mt-6 text-lg font-semibold leading-8 text-gray-300">
+              TubeCrisp will be ready in June 2023.
+              <br />
+              Don't miss out on the opportunity to be the first to gain access.
+            </p>
+            <div className="flex items-center justify-center mt-8">
+              <div className="ring-white/10 bg-white/5 ring-2 ring-indigo-500 w-full md:w-[340px] rounded-3xl p-8 xl:p-10">
+                <div className="flex items-center justify-between gap-x-4">
+                  <h3
+                    id="tier-startup"
+                    className="text-lg font-black leading-8 text-white"
+                  >
+                    TubeCrisp
+                  </h3>
+                </div>
+                <p className="flex items-baseline mt-6 gap-x-1">
+                  <span className="text-4xl font-bold tracking-tight text-white">
+                    $69
+                  </span>
+                  <span className="text-sm font-bold leading-6 text-gray-300">
+                    / lifetime
+                  </span>
+                </p>
+                <a
+                  href="https://buy.stripe.com/cN2eY50QG6EW8EMcMM"
+                  aria-describedby="tier-startup"
+                  className="bg-indigo-500 transition-colors text-white shadow-sm hover:bg-indigo-600 focus-visible:outline-indigo-500 mt-6 block rounded-md py-2 px-3 text-center text-sm font-black leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  Buy plan
+                </a>
+                <ul
+                  role="list"
+                  className="mt-8 space-y-3 text-sm leading-6 text-gray-100 xl:mt-10"
+                >
+                  <li className="flex font-bold text-left gap-x-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      className="flex-none w-5 h-6 text-white"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Unlimited content generation
+                  </li>
+                  <li className="flex font-bold text-left gap-x-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      className="flex-none w-5 h-6 text-white"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    Use TubeCrisp without the monthly subscription forever
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="mt-8 text-lg text-gray-300">
+              <div className="mb-1 font-black text-white">Refund Policy</div>
+              <div className="text-lg font-medium">
+                If you want to get your money back, just make sure to apply for a
+                refund within 30 days of the publication date. And don't worry, any
+                reason is totally fine!
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer aria-labelledby="footer-heading" className="relative">
+        <h2 id="footer-heading" className="sr-only">
+          Footer
+        </h2>
+        <div className="px-6 pt-4 pb-8 mx-auto max-w-7xl lg:px-8">
+          <div className="pt-8 border-t border-white/10 md:flex md:items-center md:justify-between">
+            <div className="flex space-x-6 md:order-2">
+              <a
+                href="https://twitter.com/taishik_"
+                target="_blank"
+                className="text-gray-500 hover:text-gray-400"
+              >
+                <span className="sr-only">Twitter</span>
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                >
+                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+              </a>
+            </div>
+            <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+              © {/* */}2023{/* */} TubeCrisp, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+  )
+}
