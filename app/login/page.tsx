328d67c8753088ee1b87950d82023ed6c1f10f6a
Allen <90570853+lxy-yz@users.noreply.github.com>
2023-06-19 08:06:53 -0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,13 @@
+import { getSession } from "@/lib/auth";
+
 import LoginForm from "./login-form";
 
-export default function LoginPage() {
+export default function LoginPage() { 
+  const session = await getSession()
+  if (session) {
+    return redirect('/discover')
+  }
+  
   return (
     <div className="mx-auto max-w-sm h-[400px] md:h-[600px] flex items-center">
       <LoginForm />
